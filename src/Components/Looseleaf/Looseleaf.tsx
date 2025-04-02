b5befa4c9a2cc23018360b41eba359f1bdf64df7
miadodgson <mariadodgson99@.com>
2025-04-02 21:09:51 -0230

    Created Looseleaf component

---
+++ src/Components/Looseleaf/Looseleaf.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import styles from './Looseleaf.module.css'
+
+
+
+const Looseleaf: React.FC = () => {
+    return (
+        <div className={styles.container}> 
+            <div className={styles.title}>
+                CSDE1 promotes miR-  <br /> 451 biogenesis 
+            </div>
+        </div>
+    )
+}
+
+export default Looseleaf
