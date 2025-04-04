01f7d4aeb719d09d9be270f67cbe91702fd82df0
miadodgson <mariadodgson99@.com>
2025-04-03 22:41:26 -0230

    Changed title font on AboutMe and modified Looseleaf component.

--- src/Components/Looseleaf/Looseleaf.tsx
+++ src/Components/Looseleaf/Looseleaf.tsx
@@ -5,9 +5,11 @@ import styles from './Looseleaf.module.css'
 
 const Looseleaf: React.FC = () => {
     return (
-        <div className={styles.container}> 
-            <div className={styles.title}>
-                CSDE1 promotes miR-  <br /> 451 biogenesis 
+        <div className={styles.container}>
+            <div className={styles.looseleaf}> 
+                <div className={styles.title}>
+                    CSDE1 promotes miR-451 biogenesis 
+                </div>
             </div>
         </div>
     )
