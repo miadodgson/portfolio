eee9ca2991604f177e6ebe22e674fd4d06867f7b
miadodgson <mariadodgson99@.com>
2025-04-12 23:19:37 -0230

    Commented out Footer styles.

--- src/TopLevel/Footer/Footer.tsx
+++ src/TopLevel/Footer/Footer.tsx
@@ -5,8 +5,8 @@ import ReactIcon from '../../Assets/Images/React.svg'
 const Footer = () => {
 	return (
 		<div className={styles.footer}>
-			<h1 className={styles.h1}>Created in React</h1>
-			<img src={ReactIcon} className={styles.icon} alt="React Icon"/>
+			{/* <h1 className={styles.h1}>Created in React</h1> */}
+			{/* <img src={ReactIcon} className={styles.icon} alt="React Icon"/> */}
 		</div>
 	)
 }
