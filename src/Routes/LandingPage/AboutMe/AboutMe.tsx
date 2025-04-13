694ea5a501a0e75cbdedb8f8354645aad0d68489
miadodgson <mariadodgson99@.com>
2025-04-12 23:22:48 -0230

    Reformatted AboutMe page.

--- src/Routes/LandingPage/AboutMe/AboutMe.tsx
+++ src/Routes/LandingPage/AboutMe/AboutMe.tsx
@@ -3,37 +3,48 @@ import styles from './AboutMe.module.css'
 import Looseleaf from "../../../Components/Looseleaf/Looseleaf"
 import img from '../../../Assets/Images/DiplomaIcon.png'
 import GradientCard from "../../../Components/GradientCard/GradientCard"
-
+import GradientText from '../../../Components/GradientText/GradientText'
 
 
 const AboutMe: React.FC = () => {
 	return (
 		<div>
 			<div className={styles.titleBox}>
-				<div className={styles.title}>Biochemist
+				<div className={styles.hello}> Hello,  I'm
 				</div>
+				<div className={styles.name}> Maria Dodgson
+				</div>
+				<GradientText
+					colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+					animationSpeed={3}
+					className="custom-class"
+					>
+					Add a splash of color!
+				</GradientText>
+				{/* <div className={styles.title}>Biochemist
+				</div> */}
 			</div>
 			<div className={styles.container}>
-				<Looseleaf />
-					<div className={styles.box}>Column 1
-					</div>
+					{/* <div className={styles.box}>Column 1
+					</div> */}
 				{/* <img src={img} alt="Achievement" className={styles.diplomaIcon} /> */}
 				<div className={styles.cardsContainer}>
 					<GradientCard
-					icon="🏆"
-					title="B Sc."
-					text="Biochemistry."
+						icon="🏆"
+						title="B Sc."
+						text=""
 					/>
 					<GradientCard
-					icon="📚"
-					title="Education"
-					text="MSc. Molecular Biology."
+						icon="📚"
+						title="Education"
+						text=""
 					/>
 					<GradientCard
-					icon="🔬"
-					title="Research"
-					text="Focused on protein folding."
+						icon="🔬"
+						title="Research"
+						text=""
 					/>
+					<Looseleaf />
 				</div>
 			</div>
 		</div>
