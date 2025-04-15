6173efcd975855cf62cff2d1980c9138e4ba5f83
miadodgson <mddodgson@mun.ca>
2025-04-15 01:36:00 -0230

    Added Lanyard, GradientText, and RotatingText to AboutMe.

--- src/Routes/LandingPage/AboutMe/AboutMe.tsx
+++ src/Routes/LandingPage/AboutMe/AboutMe.tsx
@@ -1,53 +1,42 @@
 import React from "react"
 import styles from './AboutMe.module.css'
+import Lanyard from '../../../Components/Lanyard/Lanyard'
 import Looseleaf from "../../../Components/Looseleaf/Looseleaf"
 import img from '../../../Assets/Images/DiplomaIcon.png'
 import GradientCard from "../../../Components/GradientCard/GradientCard"
-import GradientText from '../../../Components/GradientText/GradientText'
-import Lanyard from '../../../Components/Lanyard/Lanyard'
+import GradientText from '../../../Text/GradientText/GradientText'
+import RotatingText from '../../../Text/RotatingText/RotatingText'
 
 
 const AboutMe: React.FC = () => {
 	return (
 		<div>
-			<Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
+			{/* <div className={styles.lanyardElement}> */}
+			<Lanyard position={[0, 0, 40]} gravity={[0, -40, 0]} />
+			{/* </div> */}
 			<div className={styles.titleBox}>
 				<div className={styles.hello}> Hello,  I'm
 				</div>
-				<div className={styles.name}> Maria Dodgson
-				</div>
-				<GradientText
-					colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
-					animationSpeed={3}
-					className="custom-class"
-					>
-					Add a splash of color!
-				</GradientText>
-				{/* <div className={styles.title}>Biochemist
+				{/* <div className={styles.name}> Maria Dodgson
 				</div> */}
-			</div>
-			<div className={styles.container}>
-					{/* <div className={styles.box}>Column 1
-					</div> */}
-				{/* <img src={img} alt="Achievement" className={styles.diplomaIcon} /> */}
-				<div className={styles.cardsContainer}>
-					<GradientCard
-						icon="🏆"
-						title="B Sc."
-						text=""
-					/>
-					<GradientCard
-						icon="📚"
-						title="Education"
-						text=""
-					/>
-					<GradientCard
-						icon="🔬"
-						title="Research"
-						text=""
-					/>
-					<Looseleaf />
-				</div>
+				<GradientText className={styles.gradientText}>
+					Maria Dodgson
+				</GradientText>
+					<div className={styles.rotatingTextContainer}>
+						<RotatingText
+							texts={['Biochemist', 'UI Designer', 'Scientific Co-Author', 'Conservationist']}
+							mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+							staggerFrom={"last"}
+							initial={{ y: "100%" }}
+							animate={{ y: 0 }}
+							exit={{ y: "-120%" }}
+							staggerDuration={0.025}
+							splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+							transition={{ type: "spring", damping: 30, stiffness: 400 }}
+							rotationInterval={2000}
+						/>
+					</div>
+				{/* <Looseleaf /> */}
 			</div>
 		</div>
 	);
