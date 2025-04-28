e9d0e9bb11770cd5ba83c47a53efc6932dc68cdd
miadodgson <mddodgson@mun.ca>
2025-04-28 20:14:55 -0230

    Changed background image and adjusted GradientText, RotatingText, and Navbar to match the background image.

--- src/Text/GradientText/GradientText.tsx
+++ src/Text/GradientText/GradientText.tsx
@@ -11,7 +11,8 @@ interface GradientTextProps {
 
 export default function GradientText({
   children,
-  colors = ["#86f7d7", "#9f0fff", "#ff1fc7"],
+  // colors = ["#86f7d7", "#9f0fff", "#ff1fc7"],
+  colors = ["#86f7d7", "#9f0fff", "#ff99ff"],
   animationSpeed = 6,
   className = "",
   style = {},
