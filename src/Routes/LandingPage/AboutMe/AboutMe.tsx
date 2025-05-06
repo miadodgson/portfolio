f9c40acd53b3e87ccc39bb7bfd7db67832758349
miadodgson <mddodgson@mun.ca>
2025-05-06 16:50:52 -0230

    Small alignment adjustments in AboutMe and SplitText.

--- src/Routes/LandingPage/AboutMe/AboutMe.tsx
+++ src/Routes/LandingPage/AboutMe/AboutMe.tsx
@@ -35,7 +35,7 @@ const AboutMe: React.FC = () => {
 					texts={[
 							"Biochemist",
 							"UI Designer",
-							"Scientific Co-Author",
+							"Co-Author",
 							"Environmentalist",
 						]}
 					mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
