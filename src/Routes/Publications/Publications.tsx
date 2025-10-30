f2de9161e07a9f87989f40188c40244458cf8761
miadodgson <mddodgson@mun.ca>
2025-10-29 22:32:30 -0700

    added downloadable CV and linked research paper to Publications page.

--- src/Routes/Publications/Publications.tsx
+++ src/Routes/Publications/Publications.tsx
@@ -2,7 +2,9 @@ import styles from './Publications.module.css';
 import GradientLine from '../../Components/GradientLine/GradientLine';
 import Paper from '../../Assets/Images/Paper.jpeg';
 import SolidButton from '../../Components/SolidButton/SolidButton';
-
+import PearlButton from '../../Components/PearlButton/PearlButton';
+import DownloadButton from '../../Components/DownloadButton/DownloadButton';
+import CV from '../../Assets/Images/CV.pdf';
 
 const Publications = () => {
 	const handleClick = () => {
@@ -10,11 +12,25 @@ const Publications = () => {
 	};
 
 	return (
+		<div> 
+			{/*<h3 className={styles.h3}>Maria Dodgson</h3>*/}
 		<div className={styles.container}>
 			<h1 className={styles.h1}>Publications</h1>
 			<GradientLine />
+			{/*
 			<img src={Paper} className={styles.paper} alt="Publication" />
 			<SolidButton label='Read More' onClick={handleClick} />
+			*/}
+				<div>
+					<PearlButton className={styles.read} onClick={handleClick}/>
+				</div>
+				<div>
+			<h1 className={styles.h2}>Download My CV</h1></div>
+			<GradientLine />
+				<div>
+					<DownloadButton className={styles.download}/>
+				</div>
+			</div>	
 		</div>
 	);
 };
